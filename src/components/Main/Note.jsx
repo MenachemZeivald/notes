1b4330e153814b272a66b41028863cdead36cfb2
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2023-06-27 14:04:46 +0300

    add react markdown

--- src/components/Main/Note.jsx
+++ src/components/Main/Note.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import Tag from './Tag';
 
 export default function Note({ note, chooseNote }) {
+	const [noteColor, setNoteColor] = React.useState(note?.color);
 	return (
-		<NoteStyle>
+		<NoteStyle color={noteColor}>
+			<input type='color' value={noteColor} onChange={e => setNoteColor(e.target.value)} />
 			<h2>{note.title}</h2>
-			<p>{note.text}</p>
+			<ReactMarkdown>{note.text}</ReactMarkdown>
 			<div>{note.date}</div>
 			{note.tags.map((tag, i) => (
 				<Tag key={i} tagName={tag} />
@@ -17,9 +20,30 @@ export default function Note({ note, chooseNote }) {
 }
 
 const NoteStyle = styled.div`
-	background-color: ${p => p.theme.note};
+	background-color: ${p => p.color || p.theme.note};
 	border-radius: 5px;
 	padding: 10px;
 	padding: 10px;
 	margin: 10px 0;
+	position: relative;
+
+	& > input {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		border-radius: 50%;
+		height: 20px;
+		width: 20px;
+		border: none;
+		outline: none;
+		-webkit-appearance: none;
+	}
+
+	& > input::-webkit-color-swatch-wrapper {
+		padding: 0;
+	}
+	& > input::-webkit-color-swatch {
+		border: none;
+		border-radius: 50%;
+	}
 `;
